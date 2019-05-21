dc2c18ae9406eeac3f147c7b31bd470f6594d54c
LFCCarvalho <49159083+LFCCarvalho@users.noreply.github.com>
2019-05-21 11:29:23 -0300

    Atualização Personagem.h, Inventario.h
    Mudança para funções com ponteiros e acesso de objetos corrigido.

--- Headers Iniciais/Inventario.h
+++ Headers Iniciais/Inventario.h
@@ -1,12 +1,32 @@
-#ifndef INVENTORIO_H
-#define INVENTORIO_H
+#ifndef INVENTARIO_H
+#define INVENTARIO_H
+
+#include "Pocao.h"
+//#include "Arma.h"
+//#include "Armadura.h"
 
 class Inventario{
-    private:
-        float gold;
-        float pocao_vida;
-        float pocao_mana;
-        std::vector<Equipamento> equips;
+protected:
+    int gold;
+   	Pocao life_pot;
+   	Pocao stamina_pot;
+    //std::vector<Arma> unused_weapons;
+    //std::vector<Armadura> unused_armor;
+public:
+	Inventario();
+	int get_gold();
+	void set_gold();
+
+	int get_pot_life(Pocao *C);
+	int get_pot_stamina(Pocao *C);
+	int get_pot_quantity2(Pocao *C);
+	void set_pot_quantity2(Pocao *C, int num2);
+	int get_gold2();
+	void set_gold2(int num2);
+	Pocao* get_life_pointer();
+	Pocao* get_stamina_pointer();
+	
 };
 
-#endif // INVENTORIO_H
+#endif // INVENTARIO_H
+
--- Headers Iniciais/Personagem.h
+++ Headers Iniciais/Personagem.h
@@ -3,17 +3,47 @@
 
 #include<string>
 
+#include "Inventario.h"
+//#include "Armadura.h"
+//#include "Arma.h"
+
+
 class Personagem{
-    protected:
+    private:
         std::string object_name;
-        float life;
-        float deffense;
-        float attack;
-        float stamina; // (ou mana?)
+        int life;
+        int deffense;
+        int attack;
+        int stamina; // (ou mana?)
+    protected:
         Inventario inventory;
-        Armadura armor;
-        Arma weapon;
+        //Armadura armor;
+        //Arma weapon;
     public:
+    Personagem(std::string _obj_name, int _life, int _defense, int _attack, int _stamina);
+   		std::string get_object_name();
+    	void set_object_name(std::string name_change);
+    	int get_life();
+  		void set_life(int life_change);
+  		int get_deffense();
+  		void set_deffense(int def_change);
+  		int get_attack();
+  		void set_attack(int attack_change);
+  		int get_stamina();
+  		void set_stamina(int stamina_change);
+
+  		Inventario* git(); // get_inventory_pointer();
+
+	  	void use_life_potion(Inventario *B);
+	    void use_stamina_potion(Inventario *B);
+	    int get_life_pot_quantity(Inventario *B);
+	    int get_stamina_pot_quantity(Inventario *B);
+	    void set_life_pot_quantity(Inventario *B, int num1);
+	    void set_stamina_pot_quantity(Inventario *B, int num1);
+
+	    int get_gold1(Inventario *B);
+	    void set_gold1(Inventario *B, int num1);
+
 };
 
 #endif // PERSONAGEM_H
