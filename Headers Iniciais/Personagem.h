edb4f6d7c1d45b4223218c3dced5a65eadcb106b
LFCCarvalho <49159083+LFCCarvalho@users.noreply.github.com>
2019-05-25 21:09:53 -0300

    Update headers
    Adição de métodos de impressão, adição de método de acesso de ID de itens. Mudança na organização(ordem) das funções.

--- Headers Iniciais/Personagem.h
+++ Headers Iniciais/Personagem.h
@@ -10,7 +10,7 @@
 
 class Personagem{
   private:
-    std::string object_name;
+    std::string name;
     std::string casa;
     int life;
     int defense;
@@ -21,10 +21,12 @@ class Personagem{
     Arma weapon;
 
   public:
-    Personagem(std::string _obj_name, std::string _casa, int _life, int _defense, int _attack, int _stamina, Armadura _armor, Arma _weapon); // Todos os valores podem ser alterados para balancear o início de jogo;
+  	//Construtor
+    Personagem(std::string _obj_name, std::string _casa, int _life, int _defense, int _attack, int _stamina, Inventario _inventory, Armadura _armor, Arma _weapon);
 
- 		std::string get_object_name();
-  	void set_object_name(std::string name_change);
+   	//Acesso Personagem
+ 		std::string get_name();
+  	std::string get_casa();
   	int get_life();
 		void set_life(int life_change); // soma, não substitui o valor;
 		int get_defense();
@@ -35,33 +37,39 @@ class Personagem{
   	void set_stamina(int stamina_change); // soma, não substitui o valor;
 
     //Acesso Pocoes
-		void use_life_potion();
-	  void use_stamina_potion();
     int get_life_pot_quantity();
     int get_stamina_pot_quantity();
     void set_life_pot_quantity(int num);
     void set_stamina_pot_quantity(int num);
 
-    //Acesso Inventario
+    // Acesso Inventario
     int get_gold();
 	  void set_gold(int num);
+	  void display_inventory();
 
     // Acesso Armadura e Arma
-    void equip_armor(Armadura toequip);
-    void unequip_armor();
-    void equip_weapon(Arma toequip);
-    void unequip_weapon();
+    
     std::string get_armor_name();
     int get_armor_defense();
     int get_armor_id();
     int get_armor_price();
+    void display_armor();
     std::string get_weapon_name();
     int get_weapon_attack();
     int get_weapon_id();
     int get_weapon_price();
-    void display_inventory();
-    void add_armor(Armadura toequip);
+    void display_weapon();
+
+    // Funções de uso direto
+    void use_life_potion();
+	  void use_stamina_potion();
+	  void add_armor(Armadura toequip);
     void add_weapon(Arma toequip);
+	  void equip_armor(Armadura toequip);
+    void unequip_armor();
+    void equip_weapon(Arma toequip);
+    void unequip_weapon();
+   	void display_stats();
 };
 
 #endif // PERSONAGEM_H
