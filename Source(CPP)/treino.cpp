d8ba44e92217a4aef4c98084355429fa033c6c2e
Cecilia Nascimento <cecilia.pereira@dcc.ufmg.br>
2019-05-29 15:13:08 -0300

    criando funcao de treino

---
+++ Source(CPP)/treino.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "treino.h"
+
+
+void treinar(int t){
+	if(t>0){
+		//funcao batalha
+		t--;
+		//deseja treinar mais?(0/1)
+		if(a){
+			//ok
+		}
+		else{
+			break;
+		}
+	}
+	else{
+		cout << "Sem mais tentativas para treino" <<endl;
+	}
