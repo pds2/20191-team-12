355a70d032c2eb43672f06254114f2e2d334ec1a
Cecilia Nascimento <cecilia.pereira@dcc.ufmg.br>
2019-05-29 15:14:55 -0300

    definindo a main

--- program/main.cpp
+++ program/main.cpp
@@ -1,1 +1,28 @@
 #include <iostream>
+
+
+
+//declarar variaveis globais: vidas, personagem
+
+//exibir menu de escolha e textinho
+
+//coletar dados do jogador (escolher a casa)
+
+
+while(vidas > 0){
+    
+    if (personagem == dani){
+        //ganha dinheiro + vida + habilidade 
+        //menu: batalha x treinar x loja
+        //funcao de batalha
+        //menu: batalha x treinar x loja
+    }
+    else if (personagem == joao){
+        //ganha dinheiro + vida + habilidade 
+        //menu: batalha x treinar x loja
+        //funcao de batalha
+        //menu: batalha x treinar x loja
+    }
+    else 
+        //personagem invalido
+}
