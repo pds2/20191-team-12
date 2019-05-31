ab0e9f46d784479fd4efdc56f39b364c8a381095
LFCCarvalho <49159083+LFCCarvalho@users.noreply.github.com>
2019-05-31 00:37:13 -0300

    Adição de FunçõesGerais.h

---
+++ Headers Iniciais/FuncoesGerais.h
@@ -0,0 +1,10 @@
+#ifndef FUNCOES_GERAIS_H
+#define FUNCOES_GERAIS_H
+
+#include <string>
+
+void checker(int *variavel, int p1, int p2);
+std::string adjust_square(std::string to_print, int num);
+void print_square(std::string to_print1, std::string to_print2, std::string to_print3, std::string to_print4);
+
+#endif
