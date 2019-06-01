3713082d94994668542fdeeb2ac6916b879d6536
Cecilia Nascimento <cecilia.pereira@dcc.ufmg.br>
2019-06-01 08:28:17 -0300

    Criando fase 1

--- Source(CPP)/fases.cpp
+++ Source(CPP)/fases.cpp
@@ -1,1 +1,47 @@
 #include <iostream>
+#include <windows.h>
+#include <Personagem.h>
+
+
+void get_stage_game(int n){
+
+    if (n == 1){
+        cout << "~ALGUM LUGAR NO MAR DE DORNE~"
+        textcolor(YELLOW);
+        cout << "Maria: Bem Vindo a sua jornada," << player.name << ". Voce foi invocado para este mundo para combater o mal eminente que assola nossos reinos" << endl;
+        cout << "Maria: Eu sou a sarcedotisa Maria e vou lhe auxiliar na sua jornada. Na sua vida anterior voce era um otaku fracassado, mas de alguma forma lhe acharam digno de nos salvar" <<endl;
+        cout << "Voce: Err... " << endl;
+        cout << "Maria: Segundo a profecia, voce tera que passar por 7 provacoes antes de ser livre para voltar ao seu mundo. Primeiro vamos para dorne para conseguir alguns equipamentos." << endl;
+        cout << "Maria: Nao se esqueca: O inverno esta chegando e a C&A tem varias promocoes em casacos" << endl;
+        
+        //pressione alguma tecla para continuar
+        textcolor(WHITE);
+        cout << "~DORNE - LAR DA CASA MACHADO~" << endl;
+        
+        textcolor(YELLOW);
+        cout << "Maria: Aqui nos resolvemos os nosso conflitos com batalhas. Ao longo do caminho voce tambe podera treinar para aperfeicoar suas habilidades ou ganhar dinheiro" <<endl;
+        cout << "Maria: Mercadores tambem estao sempre disponiveis, neles voce pode comprar novas armas, armaduras e pocoes. Tais servicos estaram disponiveis sempre que precisar! Vamos fazer um teste:" <<endl;
+        show_menu();
+        cout << "Maria: Agora que voce ja esta preparado, podemos seguir a nossa jornada"
+
+        //pressione alguma tecla para continuar
+        cout << "??? : *Grito*" << endl;
+        cout << "Maria: Parece que alguem precisa de ajuda. Vamos ver o que esta acontecendo." << endl;
+        cout << "Narrador: Varios homens emboscavam uma mulher enquanto ela lutava com um chicote"
+        cout << "Eliana: Podem vir! Como se simples homens conseguissem me derrotar!" < endl;
+        cout << "Narrador: Um dos homens covardemente joga areia nos seus olhos e outro a apunha-la pelas costas" <<endl;
+        cout << "Maria: O que esta esperando? Voce deve ajuda-la!!" << endl;
+        // funcao batalha 
+
+        //pressione alguma tecla para continuar
+        cout << "Eliana: Eu nao precisava da sua ajuda, posso me defender sozinha" << endl;
+        cout << "Narrador:  Eliana vai embora e te deixa sozinho" << endl;
+        cout << "Maria: Parabens! O que achou da sua primeira batalha? Nao importa. Vamos, pegue o dinheiro desses bandidos e vamos embora." << endl;
+        cout << "Voce: Para onde vamos?" << endl;
+        cout << "Maria:  Vamos para a cidadela buscar mais informacoes" <<endl;
+        cout << "Narrador: Assim, seguiram para Cidade Velha, para encontrar com os sabios da cidadela" << endl;  
+
+        //limpar a tela
+    
+    }
+}
