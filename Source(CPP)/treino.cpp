b39610a2f119af2ad72b431443a4395d44d9e60b
emersongouveia <47765183+emersongouveia@users.noreply.github.com>
2019-06-06 19:12:18 -0300

    Update treino.cpp

--- Source(CPP)/treino.cpp
+++ Source(CPP)/treino.cpp
@@ -8,9 +8,9 @@
 
 
 void treinar(Personagem &heroi, Mob &npc, int t_num, int vgold_min, int vgold_max){ //t_num = quantidade de numero de treino
-																					//v_gold = variação da quantidade de gold
+										//v_gold = variação da quantidade de gold
 	if(t_num == 0){
-		std::cout << "Ta se achando a Daenerys? Voce ja matou todos os soldados na area... " << std::endl;
+		std::cout << "Ta se achando a Daniela Tavares? Voce ja matou todos os soldados na area... " << std::endl;
 		return;
 	}	
 
@@ -19,7 +19,7 @@ void treinar(Personagem &heroi, Mob &npc, int t_num, int vgold_min, int vgold_ma
 
 	do{ 
 		int gold = (rand() % vgold_max) + vgold_min; //gold varia entre a quantia máxima e minima passada
-													//vgold_max e vgold_min aumentam de acordo com o passar das fases
+							//vgold_max e vgold_min aumentam de acordo com o passar das fases
 		
 		if(batalha(heroi, npc) == 0){
 			if(heroi.get_life() > 0){
