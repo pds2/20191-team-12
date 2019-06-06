05e90c863fe1607590bdd333e5a5bc8d21d69fff
LFCCarvalho <49159083+LFCCarvalho@users.noreply.github.com>
2019-06-06 20:07:36 -0300

    Update Personagem header

--- Headers Iniciais/Personagem.h
+++ Headers Iniciais/Personagem.h
@@ -1,8 +1,8 @@
 #ifndef PERSONAGEM_H
 #define PERSONAGEM_H
 
-#include<string>
-#include<vector>
+#include <string>
+#include <vector>
 
 #include "Inventario.h"
 #include "Armadura.h"
@@ -12,7 +12,6 @@
 class Personagem{
   private:
     std::string name;
-    std::string house;
     int life;
     int defense;
     int attack;
@@ -24,10 +23,10 @@ class Personagem{
 
   public:
   	//Construtor
-    Personagem(std::string _obj_name, std::string _house, int _life, int _defense, int _attack, int _stamina, Inventario _inventory, Armadura _armor, Arma _weapon);
+    Personagem(std::string _name);
+    Personagem(std::string _name, int _life, int _defense, int _attack, int _stamina, Inventario _inventory, Armadura _armor, Arma _weapon);
    	//Acesso Personagem
  	std::string get_name();
-  	std::string get_house();
   	int get_life();
 	void set_life(int life_change); 
 	int get_defense();
