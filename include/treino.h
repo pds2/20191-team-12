cb52f569ae455e378b524c2c784b859b164f0f44
Cecilia <cecilia.pereira@dcc.ufmg.br>
2019-06-07 13:54:38 -0300

    rename

---
+++ include/treino.h
@@ -0,0 +1,15 @@
+#ifndef TREINO_H
+#define TREINO_H
+
+#include <iostream>
+#include <time.h>
+#include "treino.h"
+#include "Batalha.h"
+#include "FuncoesGerais.h"
+#include "Personagem.h"
+#include "mob.h"
+
+
+void treinar(Personagem &heroi, Mob &npc, int t_num, int vgold_min, int vgold_max);
+
+#endif 
