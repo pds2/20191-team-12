cfe18bb9166c7458eae1a042ab828b2c48e82a9f
Cecilia <cecilia.pereira@dcc.ufmg.br>
2019-06-06 22:55:31 -0300

    add fase 3

--- src/fases.cpp
+++ src/fases.cpp
@@ -2,7 +2,10 @@
 #include <Personagem.h>
 #include <Allegro.h>
 
+using namespace std
+
 bool exit(&c){
+    cout << "Deseja continuar? (1 - SIM / 0 - NAO)" << endl;
     checker(&c, 1, 0);
     if (c)
         return TRUE;
@@ -49,5 +52,52 @@ void get_stage_game(int n){
     
     }
     else if(n == 2){
+        Fase 2
 
+        cout << "\n ~CIDADE VELHA~" << endl;
+        cout << "Maria: Chegamos a cidade velha, vamos procurar alguem para nos ajudar" << endl;
+        cout << "Sabio: Sejam bem vindos. Do que precisam? Tenho que voltar a fazer nada urgentemente" << endl;
+        cout << "Maria: Queremos saber mais sobre o motivo do " << player.name << ser invocado aqui" << endl;
+        cout << " Sabio: Pois bem. Voce foi invocado para este mundo para derrotar o rei da night e impedir que o nosso reino se torne uma eterna balada sertaneja" << endl;
+        cout << "Voce: "humm..." << endl;
+        cout << "Maria: Deseja ajudar a nossa causa?" << endl;
+        cin >> op;
+
+        //exibir menu de escolha (0/1) sair/continuar
+
+        cout << "Sabio: Antes de tudo quero saber se es digno" << endl;
+
+        //chama quiz 1
+
+
+        cout << "A ameaca que procuram esta no norte. Voces devem seguir pelo nosso reino em busca de apoio." << endl;
+        cout << "Sabio: Voces devem primeiro seguir para os Jardins Altos. Ele eh chamado assim porque foi la que Michael Jordan nasceu. Ele eh muito alto." << endl;
+        cout << "Voce: Err... arigato eu acho" << endl;
+        cout << "Sabio: Nao acredito que ajudei um otaco. Vao embora daqui, tenho varios nada pra fazer. Adeus" << endl;
+
+        cout << "\n ~JARDINS ALTOS - LAR DA FAMILIA Tyrell ~ " << endl; 
+        cout << "Narrador: Chegando la encontram com Sir Loiro Jose. Eles explicam a situacao e voce eh desafio para uma batalha" << endl;
+        cout << "Sir Louro Jose: Me prove que eh digno!" << endl;
+
+        //funcao batalha 
+
+        cout << "Sir Louro Jose: Percebi que eh digno de minha ajuda." << endl;
+        cout << "Voce recebeu x moedas" << endl;
+        //somar o dinheiro do personagem
+        cout <<"Maria: Vamos continuar rumo ao norte." << endl;
+
+        //*fase++;
+    }
+    else if(n == 3){
+        
+        cout << "\n ~ARREDORES DA CIDADE DOS REIS~" << endl;
+        cout << "Maria: Acho que estamos perto da capital. Podemos passar por la para procurar apoio" << endl;
+        cout << "Guarda: Forasteiros sao proibidos" << endl;
+        cout << "Voce: Como assim? Precisamos ver o rei!!" << endl;
+        cout << "Guarda: Sofremos um ataque recentemente. Ninguem entra ate segundas ordens." << endl;
+              
+        
+        cout << "\n~CASTELO DE ROCHA - LAR DA FAMILIA LAGO~" << endl;
+        cout <<
+    }
 }
